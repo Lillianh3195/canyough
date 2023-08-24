63582b185a7a75d3dd38ea6232b6c969169b844c
Lillian Hoang <lillianh3195@csu.fullerton.edu>
2023-08-24 12:13:40 -0700

    Finished contributions

---
+++ contribution.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Would you eat a 2-inch human ALIVE for a hundred million dollars?" << endl;
+    cout << " Note that they would taste like a cheeto." << endl;
+}
